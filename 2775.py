07cc634a8a1054670704e82d756946ca426847b1
yubeen <dbqls200@gmail.com>
2021-08-31 00:41:58 +0900

    Update 2775.py

--- 2775.py
+++ 2775.py
@@ -6,3 +6,14 @@ for _ in range(t):
     k = int(input())
     n = int(input())
 
+    sum = 0
+    arr = [[0] * n for i in range(k + 1)]
+    arr[0] = list(map(lambda x: x, range(1, n + 1)))
+
+    for i in range(1, k + 1):
+        sum = 0
+        for j in range(n):
+            sum += arr[i - 1][j]
+            arr[i][j] = sum
+
+    print(arr[k][n - 1])
