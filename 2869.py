ad51bb08b0db93f909497e71c7349389f1391b1e
yubeen <dbqls200@gmail.com>
2021-09-01 23:40:37 +0900

    Update 2869.py

--- 2869.py
+++ 2869.py
@@ -1,6 +1,6 @@
 # 2869번 - 달팽이는 올라가고 싶다
 
 a, b, v = map(int, input().split())
-result = 0
+result = (v - b) / (a - b)
 
-print(result)
+print(int(result) if result == int(result) else int(result) + 1)
